d907cdd892c2dc8396a939986385b709c6ed3756
benyoon2003 <102326657+benyoon2003@users.noreply.github.com>
2022-03-28 12:51:28 -0400

    Update word_checker_class.py

--- word_checker_class.py
+++ word_checker_class.py
@@ -9,13 +9,16 @@ class WordChecker:
     def word_checker(self):
         with open('PossibleWords.txt', encoding='utf-8') as f:
             dic = {}
-            for words in f:
+            
+            #adds all the words in scrabble from the txt file into a dictionary
+            for words in f:                                         
                 bank = f.readlines()
                 for idx, ele in enumerate(bank):
                     bank[idx] = ele.replace('\n', '')
             for i, j in enumerate(bank):
                 dic[j] = i
-
+                
+            #if the word is in the dictionary created above the function returns valid
         if self.word in dic:
             return "Valid"
         else:
